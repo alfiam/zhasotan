6874ecaa8b9eccab66f02f32a34a81c6e2c70f6e
Alfia Malakshinova <itsalfia@gmail.com>
2017-07-02 02:51:59 +0600

    commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("li").on("click", function(){
+    $("li").removeClass("active");
+    $(this).addClass("active");
+  });
+  $('#carouselFade').carousel();
+  $('#buttonsearch').click(function(){
+				$('#formsearch').slideToggle( "fast",function(){
+					 $( '#content' ).toggleClass( "moremargin" );
+				} );
+				$('#searchbox').focus()
+				$('.openclosesearch').toggle();
+		});
+
+
+
+});
